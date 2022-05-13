d5a50d1f15aa308de391dd524b7882646d39764f
Angelina Podbolotova <developer.kaczmarek@yandex.ru>
2022-05-13 15:09:33 +0300

    fixed error lint, updated application icon

--- core/src/main/kotlin/ru/mobileup/core/theme/AppTheme.kt
+++ core/src/main/kotlin/ru/mobileup/core/theme/AppTheme.kt
@@ -7,13 +7,13 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = primaryDark,
     primaryVariant = Purple700,
     secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = primaryLight,
     primaryVariant = Purple700,
     secondary = Teal200
 
--- core/src/main/kotlin/ru/mobileup/core/theme/Color.kt
+++ core/src/main/kotlin/ru/mobileup/core/theme/Color.kt
@@ -2,7 +2,7 @@ package ru.mobileup.core.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val primaryLight = Color(0xFF5022FF)
+val primaryDark = Color(0XFF4718E8)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
