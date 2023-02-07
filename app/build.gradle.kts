2c7542bf60257ec359eb633ae8b3c83b75dd68f2
eg <asf>
2023-02-07 19:10:23 +0800

    Migrate from Groovy to Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,119 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("plugin.serialization")
+    kotlin("plugin.parcelize")
+    id("io.gitlab.arturbosch.detekt")
+}
+
+android {
+    val minSdkVersion: Int by rootProject.extra
+    val targetSdkVersion: Int by rootProject.extra
+
+    namespace = "ru.mobileup.template"
+    compileSdk = targetSdkVersion
+
+    defaultConfig {
+        applicationId = "ru.flawery"
+        minSdk = minSdkVersion
+        targetSdk = targetSdkVersion
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    signingConfigs {
+        getByName("debug") {
+        }
+
+        create("release") {
+        }
+    }
+
+    buildTypes {
+        debug {
+            versionNameSuffix = "-debug"
+            applicationIdSuffix = ".debug"
+            signingConfig = signingConfigs["debug"]
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+            signingConfig = signingConfigs["release"]
+        }
+    }
+
+    setFlavorDimensions(listOf("backend"))
+    productFlavors {
+        create("dev") {
+            dimension = "backend"
+            buildConfigField("String", "BACKEND_URL", "\"https://pokeapi.co/\"")
+        }
+
+        create("prod") {
+            dimension = "backend"
+            buildConfigField("String", "BACKEND_URL", "\"https://pokeapi.co/\"")
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    packagingOptions {
+        resources.excludes += "META-INF/*"
+    }
+}
+
+dependencies {
+    coreLibraryDesugaring(libs.android.desugar)
+
+    // Modules
+    implementation(project(":core"))
+    implementation(project(":features"))
+
+    // UI
+    implementation(libs.activity.compose)
+    implementation(libs.splashscreen)
+
+    // Architecture
+    implementation(libs.bundles.decompose)
+    implementation(libs.bundles.replica)
+    implementation(libs.replica.core)
+
+    // DI
+    implementation(libs.koin)
+
+    // Debugging
+    implementation(libs.timber)
+    debugImplementation(libs.bundles.hyperion)
+    debugImplementation(libs.chucker)
+    debugImplementation(libs.replica.devtools)
+
+    // Testing
+    testImplementation(libs.junit)
+    testImplementation(libs.extJunit)
+    testImplementation(libs.kotlinTest)
+    testImplementation(libs.mockWebServer)
+    testImplementation(libs.awaitility)
+    testImplementation(libs.robolectric)
+}
