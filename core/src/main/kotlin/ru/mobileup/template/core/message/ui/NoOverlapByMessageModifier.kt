5b9fcdb6ab689038716522d3924534a3811187ad
Artur Artikov <a.artikov@gmail.com>
2023-09-25 20:01:38 +0300

    Don't use currentCompositeKeyHash in noOverlapByMessage as it doesn't works as expected

--- core/src/main/kotlin/ru/mobileup/template/core/message/ui/NoOverlapByMessageModifier.kt
+++ core/src/main/kotlin/ru/mobileup/template/core/message/ui/NoOverlapByMessageModifier.kt
@@ -1,21 +1,22 @@
 package ru.mobileup.template.core.message.ui
 
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.currentCompositeKeyHash
 import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.layout.onGloballyPositioned
+import java.util.UUID
 
-val LocalMessageOffsets = staticCompositionLocalOf { mutableStateMapOf<Int, Int>() }
+val LocalMessageOffsets = staticCompositionLocalOf { mutableStateMapOf<String, Int>() }
 
 /**
  * Allows to specify that a message (see [MessageUi]) should be displayed above an UI element.
  * For example, set noOverlapByMessage() to a bottom navigation bar, so it will not be overlapped by a message popup.
  */
 fun Modifier.noOverlapByMessage(): Modifier = composed {
-    val key = currentCompositeKeyHash
+    val key = remember { UUID.randomUUID().toString() }
     val localMessageOffsets = LocalMessageOffsets.current
 
     DisposableEffect(Unit) {
