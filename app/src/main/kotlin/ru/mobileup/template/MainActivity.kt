8146d6841b3dd1243b2714af3ae06544607cbe22
Artur Artikov <a.artikov@gmail.com>
2024-02-13 12:04:50 +0300

    Use retainedComponent to preserve root component during configuration changes

--- app/src/main/kotlin/ru/mobileup/template/MainActivity.kt
+++ app/src/main/kotlin/ru/mobileup/template/MainActivity.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.arkivanov.decompose.defaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.retainedComponent
 import com.arkivanov.essenty.lifecycle.asEssentyLifecycle
 import com.arkivanov.essenty.lifecycle.doOnDestroy
 import ru.mobileup.template.core.ComponentFactory
@@ -14,9 +15,9 @@ import ru.mobileup.template.core.theme.AppTheme
 import ru.mobileup.template.features.root.createRootComponent
 import ru.mobileup.template.features.root.presentation.RootUi
 
-// Note: rootComponent survives configuration changes due to "android:configChanges" setting in the manifest.
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalDecomposeApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -27,8 +28,10 @@ class MainActivity : ComponentActivity() {
             activityProvider.detachActivity()
         }
 
-        val componentFactory = application.koin.get<ComponentFactory>()
-        val rootComponent = componentFactory.createRootComponent(defaultComponentContext())
+        val rootComponent = retainedComponent { componentContext ->
+            val componentFactory = application.koin.get<ComponentFactory>()
+            componentFactory.createRootComponent(componentContext)
+        }
 
         setContent {
             AppTheme {
