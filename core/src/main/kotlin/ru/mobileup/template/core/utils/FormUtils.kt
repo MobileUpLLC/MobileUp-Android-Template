b31bf8701fde1d856c7e1c2c3b99ef41ab1f0734
dantrap <daniil.trapeznikov@mobileup.ru>
2025-05-05 11:56:39 +0300

    Add fake controls and debounced value for `InputControl`

--- core/src/main/kotlin/ru/mobileup/template/core/utils/FormUtils.kt
+++ core/src/main/kotlin/ru/mobileup/template/core/utils/FormUtils.kt
@@ -2,6 +2,15 @@ package ru.mobileup.template.core.utils
 
 import com.arkivanov.decompose.ComponentContext
 import dev.icerock.moko.resources.desc.StringDesc
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import ru.mobileup.kmm_form_validation.control.CheckControl
 import ru.mobileup.kmm_form_validation.control.InputControl
 import ru.mobileup.kmm_form_validation.control.PickerControl
@@ -18,7 +27,7 @@ fun ComponentContext.InputControl(
     maxLength: Int = Int.MAX_VALUE,
     keyboardOptions: KeyboardOptions = KeyboardOptions(),
     textTransformation: TextTransformation? = null,
-    visualTransformation: VisualTransformation = VisualTransformation.None
+    visualTransformation: VisualTransformation = VisualTransformation.None,
 ): InputControl = InputControl(
     componentScope,
     initialText,
@@ -29,15 +38,46 @@ fun ComponentContext.InputControl(
     visualTransformation
 )
 
+@OptIn(DelicateCoroutinesApi::class)
+fun fakeInputControl(initialText: String = "") = InputControl(GlobalScope, initialText)
+
 fun <T> ComponentContext.PickerControl(
     initialSelected: T? = null,
     displayMapper: (T?) -> StringDesc?,
 ): PickerControl<T> = PickerControl(componentScope, initialSelected, displayMapper)
 
+@OptIn(DelicateCoroutinesApi::class)
+fun <T> fakePickerControl(
+    initialSelected: T? = null,
+    displayMapper: (T?) -> StringDesc? = { null },
+) = PickerControl(GlobalScope, initialSelected, displayMapper)
+
 fun ComponentContext.CheckControl(
-    initialChecked: Boolean = false
+    initialChecked: Boolean = false,
 ): CheckControl = CheckControl(componentScope, initialChecked)
 
+@OptIn(DelicateCoroutinesApi::class)
+fun fakeCheckControl(initialChecked: Boolean = false) = CheckControl(GlobalScope, initialChecked)
+
 fun ComponentContext.formValidator(
-    buildBlock: FormValidatorBuilder.() -> Unit
+    buildBlock: FormValidatorBuilder.() -> Unit,
 ): FormValidator = componentScope.formValidator(buildBlock)
+
+@OptIn(FlowPreview::class)
+fun <T> InputControl.debouncedValue(
+    scope: CoroutineScope,
+    timeoutMillis: Long = 500L,
+    transform: (String) -> T,
+): StateFlow<T> = value
+    .debounce { if (it.isBlank()) 0L else timeoutMillis }
+    .map(transform)
+    .stateIn(
+        scope = scope,
+        started = SharingStarted.Eagerly,
+        initialValue = transform(value.value)
+    )
+
+fun InputControl.debouncedValue(
+    scope: CoroutineScope,
+    timeoutMillis: Long = 500L,
+): StateFlow<String> = debouncedValue(scope, timeoutMillis) { it }
